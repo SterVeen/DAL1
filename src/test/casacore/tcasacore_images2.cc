86a173ef0bd6e8a6c641f1f2c65646b04cebe3e6
SterVeen <veen@astron.nl>
2016-07-12 17:07:36 +0200

    Update tcasacore_images2.cc

--- src/test/casacore/tcasacore_images2.cc
+++ src/test/casacore/tcasacore_images2.cc
@@ -27,13 +27,13 @@
 #include <sstream>
 
 /* casacore header files */
-#include <casa/Arrays.h>
-#include <coordinates/Coordinates/CoordinateSystem.h>
-#include <coordinates/Coordinates/Projection.h>
-#include <coordinates/Coordinates/DirectionCoordinate.h>
-#include <images/Images/HDF5Image.h>
-#include <images/Images/PagedImage.h>
-#include <images/Images/ImageFITSConverter.h>
+#include <casacore/casa/Arrays.h>
+#include <casacore/coordinates/Coordinates/CoordinateSystem.h>
+#include <casacore/coordinates/Coordinates/Projection.h>
+#include <casacore/coordinates/Coordinates/DirectionCoordinate.h>
+#include <casacore/images/Images/HDF5Image.h>
+#include <casacore/images/Images/PagedImage.h>
+#include <casacore/images/Images/ImageFITSConverter.h>
 
 using std::cout;
 using std::cerr;
